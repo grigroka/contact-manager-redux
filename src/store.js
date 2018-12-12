2ba20df8ee464d8f707e80fc298759585d55b57a
Rokas Grigaravicius <grigroka@gmail.com>
2018-12-12 13:38:40 +0200

    redux setup

--- src/store.js
+++ src/store.js
@@ -1,4 +1,6 @@
 import { createStore, applyMiddleware, compose } from 'redux';
+// redux-dev-tools dep necessary
+// import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers';
 
@@ -9,13 +11,23 @@ const initialState = {};
 const middleware = [thunk];
 
 // Init store. Use compose to apply middleware and redux dev tools
+// const store = createStore(
+//   rootReducer,
+//   initialState,
+//   composeWithDevTools(
+//     applyMiddleware(...middleware),
+//     window.__REDUX_DEVTOOL_EXTENSION__
+//       ? window.__REDUX_DEVTOOL_EXTENSION__()
+//       : f => f
+//   )
+// );
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
 const store = createStore(
   rootReducer,
   initialState,
-  compose(
-    applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOL_EXTENSION__ && window.__REDUX_DEVTOOL_EXTENSION__()
-  )
+  composeEnhancers(applyMiddleware(...middleware))
 );
 
 export default store;
